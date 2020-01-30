fda23fb81b6e9515030e5c42eec8c5282c43480d
Malignant <bae.avila@gmail.com>
2020-01-30 17:40:24 -0600

    Tse changes

--- src/pages/Votes.js
+++ src/pages/Votes.js
@@ -5,19 +5,35 @@ import confLogo from '../images/badge-header.svg';
 import TseData from '../Data/Municipales4.json';
 
 
+
 class Votes extends React.Component {
    
     state ={
-        loading: true
+        loading: true,
+        vote: true,
+        error: undefined
     }
 
     
+
+    
+    async componentDidMount(){
+        // production versión
+        const url = "http://mediosprensa.sufragio.go.cr/resultados/api/resultados";
+        const response = await fetch(url);
+        const data = await response.json();
         
+
+        //rendering data      
+        //const data =  require('../Data/Municipales4.json');
+        console.log(data);
+        console.log('test');
+    }
+    comp
         render(){
             return (
                 <div>
-                    {/* {this.state.loading ? } */}
-                    {/* https://www.youtube.com/watch?v=T3Px88x_PsA */}
+                   {this.state.loading ? <div>loading...</div> : <div>{this.state.data.numero}</div>}
                 </div>
             )
         }
